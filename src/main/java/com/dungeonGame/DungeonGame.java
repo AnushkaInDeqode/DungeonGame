1129780ad7942083c3764c2f936d76986f79d88d
AnushkaInDeqode <akesarwani@deqode.com>
2023-05-22 14:17:17 +0530

    first commit

---
+++ src/main/java/com/dungeonGame/DungeonGame.java
@@ -0,0 +1,24 @@
+package com.dungeonGame;
+class DungeonGame {
+    static int minInitialHealth(int dungeon[][], int rows, int cols) {
+
+        int matrix[][] = new int[rows][cols];
+        int m = rows, n = cols;
+
+        matrix[m - 1][n - 1] = dungeon[m - 1][n - 1] > 0 ? 1 : Math.abs(dungeon[m - 1][n - 1]) + 1;
+
+        for (int i = m - 2; i >= 0; i--)
+            matrix[i][n - 1] = Math.max(matrix[i + 1][n - 1] - dungeon[i][n - 1], 1);
+        for (int j = n - 2; j >= 0; j--)
+            matrix[m - 1][j] = Math.max(matrix[m - 1][j + 1] - dungeon[m - 1][j], 1);
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                int minPointsOnExit = Math.min(matrix[i + 1][j], matrix[i][j + 1]);
+                matrix[i][j] = Math.max(minPointsOnExit - dungeon[i][j], 1);
+            }
+        }
+
+        return matrix[0][0];
+    }
+}
---
+++ src/main/java/com/dungeonGame/DungeonGameMain.java
@@ -0,0 +1,19 @@
+package com.dungeonGame;
+
+public class DungeonGameMain {
+
+    public static void main(String args[]) {
+
+        DungeonGame dungeonGame=new DungeonGame();
+
+        int rows = 3, cols = 3;
+        int dungeon[][] = {
+                            { -2, -3, 3 },
+                            { -5, -10, 1 },
+                            { 10, 30, -5 }
+                        };
+
+        System.out.println("Minimum Initial Points Required: "
+                                    + dungeonGame.minInitialHealth(dungeon, rows, cols));
+    }
+}
